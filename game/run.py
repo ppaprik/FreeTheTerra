6f3ff0003f1262e200b139d23ec4618b1702dbb9
psychotronixx <sabo.patrik1@gmail.com>
2025-01-27 23:41:22 +0100

    added pseudo physics & draw update

--- game/run.py
+++ game/run.py
@@ -1,35 +1,96 @@
 import pygame
-import math
+import threading
+import time
 
-pygame.init()
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# MAIN GAME CLASS
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Drawing Shapes")
+class Game():
+    pygame.init()
+    pygame.font.init()
 
-clock = pygame.time.Clock()
 
-delta_time: int = 0
+    #----------------------------------------------------------------------------------------------------
+    # VARIABLES
+    
+    # GAME
+    running: bool = True
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # DEFAULT PATH TO GAME (IF GAME IS RUNNED FROM ANOTHER LOCATION THIS MUST BE CHANGED)
+    main_path: str = "./game/"
 
-    screen.fill((0, 0, 0))  # Clear the screen
+    # EVERYTHING FOR WINDOW
+    screen: pygame.surface.Surface = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+    pygame.display.set_icon(pygame.image.load(f"{main_path}assets/sprites/crown.png"))
+    pygame.display.set_caption("FreeTheTerra")
 
-    # Drawing various shapes
-    pygame.draw.line(screen, (255, 255, 255), (100, 100), (200, 200), 5)
-    pygame.draw.rect(screen, (255, 0, 0), (50, 50, 100, 100))
-    pygame.draw.circle(screen, (0, 255, 0), (300, 300), 50)
-    pygame.draw.ellipse(screen, (0, 0, 255), (400, 50, 150, 100))
-    pygame.draw.polygon(screen, (255, 255, 0), [(150, 50), (200, 150), (100, 150)])
-    pygame.draw.arc(screen, (255, 0, 255), (500, 50, 150, 100), 0, math.pi, 5)
-    pygame.draw.lines(screen, (0, 255, 255), False, [(300, 400), (350, 450), (400, 400)], 3)
-    pygame.draw.aaline(screen, (255, 165, 0), (600, 50), (750, 200), 1)
+    # COLORS
+    WHITE: tuple[int, int, int] = (255, 255, 255)
+    BLACK: tuple[int, int, int] = (0, 0, 0)
 
-    pygame.display.flip()
+    # TEXT
+    font: pygame.font.Font = pygame.font.Font(None, 20)
+    text: pygame.surface.Surface = font.render("SAMPLE TEXT", True, WHITE, None)
 
-    delta_time = clock.tick(60) / 1000
 
-pygame.quit()
+    #----------------------------------------------------------------------------------------------------
+    # INIT
+
+    def __init__(self):
+        pass
+
+
+    #----------------------------------------------------------------------------------------------------
+    # START OF PROGRAM
+
+    def start(self):
+        physics_thread = threading.Thread(target=self.physics_update).start()
+        self.draw_update()
+
+
+    #----------------------------------------------------------------------------------------------------
+    # UPDATES
+
+    def draw_update(self):
+        delta_time: float = 0
+        draw_delta_time: float = 0
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            current_time: float = time.time()
+            draw_delta_time += delta_time
+
+            if draw_delta_time >= 0.016:
+                fps = 1 / draw_delta_time
+                print(f"Draw FPS: {fps:.2f} \t Draw delta: {draw_delta_time:.4f}")
+                draw_delta_time = 0
+
+            delta_time = round((time.time() - current_time), 4)
+
+
+    def physics_update(self):
+        delta_time: float = 0
+        physics_delta_time: float = 0
+
+        while self.running:
+            current_time: float = time.time()
+            physics_delta_time += delta_time
+
+            if physics_delta_time >= 0.001:
+                fps = 1 / physics_delta_time
+                print(f"Physics FPS: {fps:.2f} \t Physics delta: {physics_delta_time:.4f}")
+                physics_delta_time = 0
+
+            delta_time = round((time.time() - current_time), 4)
+
+            
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# EXECUTE
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
